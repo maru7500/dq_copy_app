60bbb3a2654fbf7ea0866f167718bf006ec02fe9
maru7500 <appleapple7500>
2020-03-13 23:37:18 +0900

    change12

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # userクラスが作成された後に、後述のメソッドが発動
 
   # 外部のIDを持っている場合はbelong_to,持っていない場合はhas_oneという認識でOK
-
   has_one :brave, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -14,7 +13,6 @@ class User < ApplicationRecord
   def create_initial_setting
     create_brave!(initial_brave)
   end
-
 
   private 
 
